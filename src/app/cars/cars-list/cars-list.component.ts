fee1631645981eb8ce695d11c2ee7d91045e594a
Kamil Adamuszek <kamil.adamuszek@makolab.com>
2018-05-16 19:52:55 +0200

    @ViewChild, ngAfterViewInit

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {Car} from '../models/car';
+import {TotalCostComponent} from '../total-cost/total-cost.component';
 
 @Component({
   selector: 'app-cars-list',
@@ -7,7 +8,8 @@ import {Car} from '../models/car';
   styleUrls: ['./cars-list.component.less'],
   encapsulation: ViewEncapsulation.None
 })
-export class CarsListComponent implements OnInit {
+export class CarsListComponent implements OnInit, AfterViewInit {
+  @ViewChild('totalCostRef') totalCostRef: TotalCostComponent;
   totalCost: number;
   grossCost: number;
   cars: Car[] = [
@@ -59,13 +61,21 @@ export class CarsListComponent implements OnInit {
     this.countTotalCost();
   }
 
+  ngAfterViewInit() {
+    this.totalCostRef.showGross();
+  }
+
+  showGross(): void {
+    this.totalCostRef.showGross();
+  }
+
   countTotalCost(): void {
     this.totalCost = this.cars
       .map((car) => car.cost)
       .reduce((prev, next) => prev + next);
   }
 
-  onShownGroos(grossCost: number): void{
+  onShownGroos(grossCost: number): void {
     this.grossCost = grossCost;
   }
 }
