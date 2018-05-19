c02d630bb056579780b963ae50ebcbfd24353e68
Kamil Adamuszek <kamil.adamuszek@makolab.com>
2018-05-19 12:42:27 +0200

    first route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {CarsModule} from './cars/cars.module';
 import {CarsService} from './cars/cars.service';
 import {HttpModule} from '@angular/http';
 import {CoreModule} from './core-module/core.module';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {CarsListComponent} from './cars/cars-list/cars-list.component';
 
 
 @NgModule({
@@ -15,11 +18,16 @@ import {CoreModule} from './core-module/core.module';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     CarsModule,
     HttpModule,
-    CoreModule
+    CoreModule,
+    RouterModule.forRoot([
+      {path: 'cars', component: <any> CarsListComponent}
+    ])
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/cars/cars.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {Car} from './models/car';
+import {Observable} from 'rxjs/Observable';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class CarsService {
+  private apiUrl = 'http://localhost:3000/api/cars';
+
+  constructor(private http: Http) {
+  }
+
+  getCars(): Observable<Car[]> {
+    return this.http.get(this.apiUrl)
+      .map((res) => res.json());
+  }
+}
